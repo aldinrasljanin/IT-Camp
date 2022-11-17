08f8dc8d5c790a9c27dd900e407d07a94239d9f3
aldinrasljanin <rasljaninaldin2005@gmail.com>
2022-11-17 12:28:37 +0100

    Tipovi podataka, komentari, sintaksa.

---
+++ JavaScript/1.Cas/app.js
@@ -0,0 +1,28 @@
+console.log("pozdrav iz konzole");
+// alert("pozdrav");
+// document.write("Pozdrav");
+//Komentar
+
+console.log(5 + 5);
+
+// Tipovi podataka
+// PROSTI TIPOVI PODATAKA
+// 1. String    -> predstavlja neki tekst.
+// 2. Number    -> predstavlja brojevnu vrednost odnosno omogucava da obavimo matematicke operacije.
+// 3. Boolean   -> predstavlja neku logicku vrednost.
+// 4. Undefined -> javlja da vrednost nije definisana.
+// 5. Null      -> javlja da vrednost ne postoji.
+// 6. Symbol    -> zastita podataka
+
+var tekst = "Pozdrav";
+var nesto = 5;
+console.log(tekst);
+
+var x = 5;
+var y = 10;
+var z = x + y;
+console.log("Vas zbir je " + z);
+console.log(x + y);
+console.log(x - y);
+console.log(x != y);
+console.log(x == y);
