5fb2ac3f435826a071cc4942fc3562521b765265
aldinrasljanin <rasljaninaldin2005@gmail.com>
2022-12-03 11:24:53 +0100

    Petlje

---
+++ JavaScript/5.Cas/app.js
@@ -0,0 +1,24 @@
+// for (let i = 1; i < 5; i++) {
+//   console.log("hello world", i);
+// }
+
+// for (let i = 10; i >= 0; i--) {
+//   console.log("hello world", i);
+// }
+
+// let text = "";
+// let i = 0;
+
+// while (i < 11) {
+//   text += "The number is " + i,;
+//   i++;
+//   console.log(text);
+// }
+
+// Zadatak
+// printati brojeve koji su deljivi sa 2. Petlja pocinje od 1 zavrsava do 10.
+for (let i = 1; i <= 10; i++) {
+  if (i % 2 == 0) {
+    console.log(i);
+  }
+}
