6ba0b4cdb31b8edd1505f5ac32f25eb7f23e4a61
aldinrasljanin <rasljaninaldin2005@gmail.com>
2022-12-10 12:16:21 +0100

    Funkcije zadaci

---
+++ JavaScript/9.Cas/app.js
@@ -0,0 +1,13 @@
+// 1. Napisati f-ju koja vraca kvadrat datih brojeva.
+const kvadratbroja = (broj) => {
+  return broj * broj;
+};
+console.log(kvadratbroja(2));
+console.log(kvadratbroja(9));
+console.log(kvadratbroja(15));
+
+// 2. Napisati f-ju koja izracunava povrsinu datog pravougaonika.
+const povrsinaPravougaonika = (a, b) => {
+  return a * b;
+};
+console.log(povrsinaPravougaonika(2, 5));
