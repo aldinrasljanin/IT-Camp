81aa08683c1f7f3d47c3aaed45892e3031a188b6
aldinrasljanin <rasljaninaldin2005@gmail.com>
2023-01-03 21:28:45 +0100

    Dom

---
+++ JavaScript/12.Cas/app.js
@@ -0,0 +1,8 @@
+const x = document.getElementById("Img");
+const y = (document.getElementById("text").style.color = "red");
+const z = (document.getElementById("text").style.fontFamily = "sans-serif");
+
+// Napraviti dugme koje kad se klikne promeni boju nekog teksta u plavu
+function changeColor() {
+  document.getElementById("text2").style.color = "blue";
+}
