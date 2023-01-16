08fa53aa3f0048676aa9f3ff4b2c2c4f29b0602e
aldinrasljanin <rasljaninaldin2005@gmail.com>
2023-01-16 22:24:58 +0100

    Register and Login Form Validation

--- JavaScript/Validation/Login.js
+++ JavaScript/Validation/Login.js
@@ -6,7 +6,11 @@ let confirmPassword = document.forms["form"]["confirmPassword"];
 
 let email_error = document.getElementById("email_error");
 let pass_error = document.getElementById("password_error");
+let form = document.querySelector(".login_form");
 
+form.addEventListener("submit", (e) => {
+  e.preventDefault();
+});
 const validated = () => {
   if (email.value.length < 9) {
     email.style.border = "1px solid red";
@@ -25,7 +29,7 @@ const validated = () => {
     confirmPassword.focus();
     return false;
   }
-  location.href = "/JavaScript/Validation/Home.html";
+  location.href = "/Validation/Home.html";
 };
 
 const email_Verify = () => {
@@ -43,6 +47,45 @@ const password_Verify = () => {
     return true;
   }
 };
+
+const snowflakes = {
+  particles: {
+    number: {
+      value: 100,
+    },
+    shape: {
+      type: "circle",
+    },
+    size: {
+      value: 10,
+      random: true,
+    },
+    line_linked: {
+      enable: false,
+    },
+    move: {
+      enable: true,
+      speed: 2,
+      direction: "bottom",
+      straight: false,
+    },
+  },
+  interactivity: {
+    detect_on: "canvas",
+    events: {
+      onhover: {
+        enable: false,
+      },
+    },
+    modes: {
+      push: {
+        particles_nb: 12,
+      },
+    },
+  },
+};
+
+particlesJS("particles-js", snowflakes);
 
 email.addEventListener("textInput", email_Verify);
 password.addEventListener("textInput", password_Verify);
