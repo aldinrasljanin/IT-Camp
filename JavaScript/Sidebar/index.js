ad5451a457bada51dc130082bc81cc04d3eaa10c
aldinrasljanin <rasljaninaldin2005@gmail.com>
2023-01-24 12:41:50 +0100

    Sidebar

---
+++ JavaScript/Sidebar/index.js
@@ -0,0 +1,7 @@
+function btn_open() {
+  document.getElementById("mySidebar").style.display = "block";
+}
+
+function btn_close() {
+  document.getElementById("mySidebar").style.display = "none";
+}
