8a831141d6bc378c5111faa08bcb6bd7f14a2748
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-02-18 11:28:39 +0100

    FizzBuzz Challenge

--- JavaScript/Homework/app.js
+++ JavaScript/Homework/app.js
@@ -1,14 +1,8 @@
-// for petlja
-// i ide do 100
-// ako je i deljiv sa 3 console.log("fizz")
-// ako je i deljiv sa 5 console.log("buzz")
-// ako je i deljiv i sa 3 i sa 5 console.log("fizzbuzz")
-
-// for loop
-// i goes up to 100
-// if it is also divisible by 3 console.log("fizz")
-// if it is also divisible by 5 console.log("buzz")
-// if it is divisible by both 3 and 5 console.log("fizzbuzz")
+// for petlja- for loop
+// i ide do 100- i goes up to 100
+// ako je i deljiv sa 3 console.log("fizz")- if it is also divisible by 3 console.log("fizz")
+// ako je i deljiv sa 5 console.log("buzz")- if it is also divisible by 5 console.log("buzz")
+// ako je i deljiv i sa 3 i sa 5 console.log("fizzbuzz")- if it is divisible by both 3 and 5 console.log("fizzbuzz")
 
 for (let i = 1; i <= 100; i++) {
   if (i % 3 === 0 && i % 5 === 0) {
