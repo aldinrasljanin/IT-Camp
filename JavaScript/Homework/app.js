6f9ddf9675d0a43f40789848031d6d2441f8e632
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-02-21 00:06:39 +0100

    Homework

--- JavaScript/Homework/app.js
+++ JavaScript/Homework/app.js
@@ -15,3 +15,39 @@ for (let i = 0; i <= 100; i++) {
     console.log(i);
   }
 }
+
+const positiveNumber = [6, 5, 4, 3, 2, 0, -1, -2];
+var reversedArray = [];
+
+for (let i = positiveNumber.length - 1; i >= 0; i--) {
+  console.log(positiveNumber[i], `${i} index elementa niza`);
+  reversedArray.push(positiveNumber[i]);
+}
+console.log(positiveNumber);
+console.log(reversedArray);
+
+const numbers = [3, 5, 7, 32, 67, 22];
+let najveciBroj = 0;
+
+for (let i = 0; i < numbers.length; i++);
+{
+  if (numbers[i] > najveciBroj) {
+    najveciBroj = numbers[i];
+  }
+}
+console.log(najveciBroj);
+
+console.log(Math.max.apply(null, numbers));
+
+// DOMACI
+// reverse word
+let firstTask = "laptop"; //potpal
+let secondTask = "autobus"; //subotau
+
+word1 = [...firstTask].reverse().join(" ");
+word2 = [...secondTask].reverse().join(" ");
+
+console.log(firstTask);
+console.log(word1);
+console.log(secondTask);
+console.log(word2);
