cbf49e5da9317cb69fe22a55f9526c6389c4c9be
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-02-28 11:23:12 +0100

    Object with filter, map and reduce methods

--- JavaScript/15.Cas/app.js
+++ JavaScript/15.Cas/app.js
@@ -19,32 +19,72 @@ const ourClass = [
   {
     name: "Aldin",
     lastName: "Rasljanin",
-    age: 17,
+    age: 52,
+    school: {
+      class: "III-3",
+      name: "tehnicka",
+    },
   },
   {
-    name: "Danilo",
-    lastName: "Petrovic",
+    name: "Nerma",
+    lastName: "Hot",
     age: 17,
+    school: {
+      class: "III-3",
+      name: "gimnazija",
+    },
   },
   {
     name: "Seid",
     lastName: "Alomerovic",
-    age: 19,
+    age: 25,
+    school: {
+      class: "III-3",
+      name: "tekstilna",
+    },
   },
   {
     name: "Ihsan",
     lastName: "Curic",
-    age: 20,
+    age: 23,
+    school: {
+      class: "III-3",
+      name: "ekonomska",
+    },
   },
 ];
-// callbackfn argument neke nove funkcije
-console.log(ourClass);
+
+// console.log(ourClass);
 // console.log(ourClass[2].lastName);
 // console.log(ourClass[3].name);
 // console.log(ourClass[1].name);
 
-// ourClass.map((el) => console.log(el));
+// console.log(ourClass);
 // ourClass.map((el) => console.log(el.lastName));
+// const adultPersons = ourClass.filter((el) => el.school.name === "ekonomska");
 
-const adultPerson = ourClass.filter((el) => el.age > 18);
-console.log(adultPerson);
+// console.log(adultPersons);
+// const newSomething = ourClass.map((el) => {
+//   return {
+//     name: el.name,
+//   };
+// });
+// console.log(newSomething);
+
+// const ashdjkja = [2, 3, 5, 6, 78, 8, 64, 5, 5];
+
+// let sum = ashdjkja.reduce((a, b) => a + b);
+
+// console.log(sum);
+
+// let sum = ourClass.reduce((prev, curr) => (prev += +curr.age));
+// console.log(sum);
+
+let ages = [];
+const ageFromObjects = ourClass.map((el) => {
+  ages.push(el.age);
+});
+console.log(ages);
+// console.log(ageFromObjects);
+let sum = ages.reduce((prev, curr) => prev + curr);
+console.log(sum / ages.length);
