61133f340964da9be9d0d326e7b0a79f2cfcb24d
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-03-06 23:54:04 +0100

    Practice task with functions and methods

--- JavaScript/17.Cas/app.js
+++ JavaScript/17.Cas/app.js
@@ -4,37 +4,37 @@
 // Treci deo. U drigom pomocnom nizu umetnuti na prva dva mesta 10 i 20.
 // Funkcija na kraju treba da vrati dva napravljena pomocna niza.
 
-// const DATA1 = [2, 26, 38, 75, 11, 29];
-// const DATA2 = [4, 6, 5, 10, 33, 52, 75];
+const DATA1 = [2, 26, 38, 75, 11, 29];
+const DATA2 = [4, 6, 5, 10, 33, 52, 75];
 
-// const firstAssignament = (array1, array2) => {
-//   const combined = array1.concat(array2); // Prvi deo
-//   const filteredArray = combined.filter((el) => el % 2 === 0); // Drugi deo
-//   filteredArray.unshift(10, 20); // Treci deo
-//   return combined, filteredArray; // Kraj
-// };
-// console.log(firstAssignament(DATA1, DATA2));
+const firstAssignament = (array1, array2) => {
+  const combined = array1.concat(array2); // Prvi deo
+  const filteredArray = combined.filter((el) => el % 2 === 0); // Drugi deo
+  filteredArray.unshift(10, 20); // Treci deo
+  return combined, filteredArray; // Kraj
+};
+console.log(firstAssignament(DATA1, DATA2));
 
 /// 2.Zadatak ///
 // Napraviti novi niz koji sadrzi kvadrate elemenata postojeceg niza:
 
-// const numbers2 = [2, 5, 10, 3, 4, 8];
-// const powNumbers = (array) => {
-//   let powedNumbers = array.map((el) => el * el);
-//   return powedNumbers;
-//   return array.map((el) => el * el); // Skraceni deo
-// };
-// const shortPow = (array) => array.map((el) => el * el); // Laksi nacin
-// console.log(shortPow(numbers2));
-// console.log(powNumbers(numbers2));
+const numbers2 = [2, 5, 10, 3, 4, 8];
+const powNumbers = (array) => {
+  let powedNumbers = array.map((el) => el * el);
+  return powedNumbers;
+  return array.map((el) => el * el); // Skraceni deo
+};
+const shortPow = (array) => array.map((el) => el * el); // Laksi nacin
+console.log(shortPow(numbers2));
+console.log(powNumbers(numbers2));
 
 /// 3.Zadatak ///
 // Napraviti novi niz koji sadrzi kvadratne korene elemenata postojeceg niza.
-// const numbers = [4, 9, 16, 25, 36];
-// Math.sqrt();
-// const squareNumbers = (array) => array.map((el) => Math.sqrt(el));
-// console.log(numbers);
-// console.log(squareNumbers(numbers));
+const numbers = [4, 9, 16, 25, 36];
+Math.sqrt();
+const squareNumbers = (array) => array.map((el) => Math.sqrt(el));
+console.log(numbers);
+console.log(squareNumbers(numbers));
 
 /// 5.Zadatak ///
 // Napraviti funkciju koja pravi niz koji ce iz postojeceg napraviti novi uz ispunjenje uslova:
