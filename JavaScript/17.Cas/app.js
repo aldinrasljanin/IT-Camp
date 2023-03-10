5310900f109a6c5ffd2c7342740f9c2c3e134ec5
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-03-10 21:37:11 +0100

    Practice task with Map, Filter and Reduse Methods

--- JavaScript/17.Cas/app.js
+++ JavaScript/17.Cas/app.js
@@ -117,7 +117,7 @@
 const years = [3, 6, 10, 82, 93, 105, 42, 28, 52, 50, 51, 22, 83];
 
 const averageAge = (yearsArray) =>
-  yearsArray.reduce((curr, prev) => curr + prev, 0) / yearsArray.length;
+  yearsArray.reduce((acc, year) => acc + year, 0) / yearsArray.length;
 
 // 12. Zadatak
 // Write a function that converts an array of values from miles to kilometres using the map method.
