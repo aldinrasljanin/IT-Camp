cbe6f6d1285948f76a6141f88c5f9a7dca997d56
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-03-11 12:32:54 +0100

    Exercise

--- JavaScript/18.cas/app.js
+++ JavaScript/18.cas/app.js
@@ -2,13 +2,13 @@
 // Prebrojati koliko se puta u unetom stringu pojavljuje slovo "M" ( i veliko i malo).
 // Npr. za "Mama ima momu", dobija se rezultat 5.
 
-const string = "Mama ima momu";
-function checkLetters(string) {
-  let start = 0;
-  for (let i = 0; i < string.length; i++) {
-    if (string[i] === "M" || string[i] === "m") {
-      brojac++;
-    }
-  }
-  return brojac;
-}
+// const string = "Mama ima momu";
+// function checkLetters(string) {
+//   let start = 0;
+//   for (let i = 0; i < string.length; i++) {
+//     if (string[i] === "M" || string[i] === "m") {
+//       start++;
+//     }
+//   }
+//   return start;
+// }
