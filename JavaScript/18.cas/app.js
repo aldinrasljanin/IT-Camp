ad570f7003acd7ac17db423b75140b4b1ca1a869
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-03-12 11:58:59 +0100

    Exercise

--- JavaScript/18.cas/app.js
+++ JavaScript/18.cas/app.js
@@ -19,21 +19,21 @@
 // Ispitati da li u unetom stringu ima vise malih ili velikih slova.
 // Slova koja se koriste u stringu pripadaju iskljucivo engleskoj abecedi.
 
-let stringNeki = "Ako JA SAD ovako PISEM koliko CE MALIH slova DA ima";
+// let stringNeki = "Ako JA SAD ovako PISEM koliko CE MALIH slova DA ima";
 
-const checkCases = (string) => {
-  let malaSlova = 0;
-  let velikaSlova = 0;
+// const checkCases = (string) => {
+//   let malaSlova = 0;
+//   let velikaSlova = 0;
 
-  for (let i = 0; i < string.length; i++) {
-    if (string[i] !== " ") {
-      if (string[i] === string[i].toUpperCase()) {
-        velikaSlova++;
-      } else malaSlova++;
-    }
-  }
+//   for (let i = 0; i < string.length; i++) {
+//     if (string[i] !== " ") {
+//       if (string[i] === string[i].toUpperCase()) {
+//         velikaSlova++;
+//       } else malaSlova++;
+//     }
+//   }
 
-  return `Malih slova ima ${malaSlova}, a velikih ${velikaSlova}`;
-};
+//   return `Malih slova ima ${malaSlova}, a velikih ${velikaSlova}`;
+// };
 
 console.log(checkCases(stringNeki));
