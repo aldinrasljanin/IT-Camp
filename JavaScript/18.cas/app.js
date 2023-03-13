ec68eb52cbd0d8c516d18fdc7fccdfe95a2a0e29
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-03-13 19:57:17 +0100

    Exercise

--- JavaScript/18.cas/app.js
+++ JavaScript/18.cas/app.js
@@ -23,8 +23,8 @@ console.log(checkLetter(string));
 let stringNeki = "Ako JA SAD ovako PISEM koliko CE MALIH slova DA ima";
 
 const checkCases = (string) => {
-  let malaSlova = 0; // pocetak brojaca malih slova
-  let velikaSlova = 0;
+  let malaSlova = 0; // pocetna vrednost brojaca malih slova
+  let velikaSlova = 0; // pocetak brojaca velikih slova
 
   for (let i = 0; i < string.length; i++) {
     if (string[i] !== " ") {
