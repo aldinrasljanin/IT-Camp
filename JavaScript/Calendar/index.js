a716f04bca7fc8bdef279a3a797dccb67d79500e
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-03-13 20:15:53 +0100

    Calendar

--- JavaScript/Calendar/index.js
+++ JavaScript/Calendar/index.js
@@ -3,3 +3,8 @@ const calendarBody = document.getElementById("calendar__body");
 const monthName = document.getElementById("name");
 const previousMonthButton = document.getElementById("previous__month");
 const nextMonthButton = document.getElementById("next__month");
+
+// Set the initial date to the current month
+let currentDate = new Date();
+let currentMonth = currentDate.getMonth();
+let currentYear = currentDate.getFullYear();
