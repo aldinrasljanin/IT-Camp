1f25ac2f3f1eb3cdd7ba69b7466f7bdaad9c55ef
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-03-13 20:15:42 +0100

    Calendar

---
+++ JavaScript/Calendar/index.js
@@ -0,0 +1,5 @@
+// Get the calendar elements
+const calendarBody = document.getElementById("calendar__body");
+const monthName = document.getElementById("name");
+const previousMonthButton = document.getElementById("previous__month");
+const nextMonthButton = document.getElementById("next__month");
