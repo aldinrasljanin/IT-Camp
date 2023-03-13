b44eb714b7849f52bae986a24fbe114e4046af69
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-03-13 20:16:18 +0100

    Calendar

--- JavaScript/Calendar/index.js
+++ JavaScript/Calendar/index.js
@@ -8,3 +8,17 @@ const nextMonthButton = document.getElementById("next__month");
 let currentDate = new Date();
 let currentMonth = currentDate.getMonth();
 let currentYear = currentDate.getFullYear();
+
+// Function to render the calendar
+function renderCalendar() {
+    // Get the number of days in the current month
+    const daysInMonth = new Date(currentYear, currentMonth + 1, 0).getDate();
+  
+    // Get the first day of the current month
+    const firstDayOfMonth = new Date(currentYear, currentMonth, 1).getDay();
+  
+    // Clear the calendar body
+    calendarBody.innerHTML = '';
+  
+    // Set the month name
+    monthName.textContent = new Intl.DateTimeFormat('en-US', { month: 'long', year: 'numeric' }).format(currentDate);
