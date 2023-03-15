3a427649fcc92d579960cde719c5233a1b18b34b
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-03-15 01:02:21 +0100

    Object Oriented Programming

--- JavaScript/19.Cas/app.js
+++ JavaScript/19.Cas/app.js
@@ -76,6 +76,7 @@ const pas = new Dog("aw aw");
 // console.log(pas.writeAges());
 //nasljedjivanje, enkapsulacija, abstrakcija, polimorfizam
 
+//Example for static name
 class School {
   static name = "NIT";
   static nekaFun() {
