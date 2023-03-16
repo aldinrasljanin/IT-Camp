821d0a6b8060514e262f329cd9a106a98c781bb0
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-03-16 12:06:59 +0100

    Classes in JavaScript exercise

--- JavaScript/20.Cas/app.js
+++ JavaScript/20.Cas/app.js
@@ -1,5 +1,7 @@
 console.log("Exercise for javascript classes");
 
+// poc brz 120,f-ja za ubrzanje za 20 kad kaze uspori smanji brz za 20 i kad ukoci da smnanji brz na 0
+// clg kada se pozove bilo koja da clga i f-ju i brz
 class Auto {
   brand;
   speed;
