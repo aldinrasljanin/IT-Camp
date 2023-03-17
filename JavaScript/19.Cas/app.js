43f02e5be71f3d4933da8916b329d198b3219c87
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-03-17 21:59:14 +0100

    Object Oriented Programming

--- JavaScript/19.Cas/app.js
+++ JavaScript/19.Cas/app.js
@@ -114,7 +114,7 @@ let skola = new School(30);
 
 // Example for private class //
 class Shop {
-  #profit = 30; // To get this argument we have
+  #profit = 30; // To get this argument we have to
   proizvod = 40;
 
   getProfit() {
