8daa25a84c3570716d4b24631bb9d2e5f2f2cfd6
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-03-19 23:48:19 +0100

    Classes in JavaScript exercise

--- JavaScript/20.Cas/app.js
+++ JavaScript/20.Cas/app.js
@@ -1,64 +1,64 @@
-console.log("Exercise for javascript classes");
+// console.log("Exercise for javascript classes");
 
-// pocetna brzina 120,funkcija za ubrzanje 20 kad kaze uspori smanji brzina za 10 i kad ukoci da brzina bude 0
-// clg kada se pozove bilo koja da clga i f-ju i brz
-class Car {
-  brand;
-  #speed = 120;
+// // pocetna brzina 120,funkcija za ubrzanje 20 kad kaze uspori smanji brzina za 10 i kad ukoci da brzina bude 0
+// // clg kada se pozove bilo koja da clga i f-ju i brz
+// class Car {
+//   brand;
+//   #speed = 120;
 
-  constructor(brandName) {
-    this.brand = brandName;
-  }
+//   constructor(brandName) {
+//     this.brand = brandName;
+//   }
 
-  getSpeed() {
-    return this.#speed;
-  }
+//   getSpeed() {
+//     return this.#speed;
+//   }
 
-  fastUp() {
-    this.#speed += 20;
-  }
+//   fastUp() {
+//     this.#speed += 20;
+//   }
 
-  slowDown() {
-    this.#speed -= 10;
-  }
+//   slowDown() {
+//     this.#speed -= 10;
+//   }
 
-  stop() {
-    this.#speed = 0;
-  }
-}
+//   stop() {
+//     this.#speed = 0;
+//   }
+// }
 
-const myNewCar = new Car("Audi");
-// console.log(myNewCar.getSpeed());
-// myNewCar.fastUp();
-// console.log(myNewCar.getSpeed());
-// myNewCar.fastUp();
-// console.log(myNewCar.getSpeed());
-// myNewCar.fastUp();
-// console.log(myNewCar.getSpeed());
-// myNewCar.fastUp();
-// console.log(myNewCar.getSpeed());
+// const myNewCar = new Car("Audi");
+// // console.log(myNewCar.getSpeed());
+// // myNewCar.fastUp();
+// // console.log(myNewCar.getSpeed());
+// // myNewCar.fastUp();
+// // console.log(myNewCar.getSpeed());
+// // myNewCar.fastUp();
+// // console.log(myNewCar.getSpeed());
+// // myNewCar.fastUp();
+// // console.log(myNewCar.getSpeed());
 
-// myNewCar.fastUp();
+// // myNewCar.fastUp();
 
-class Bus extends Car {
-  seats = 50;
+// class Bus extends Car {
+//   seats = 50;
 
-  constructor(brand) {
-    super(brand);
-  }
-}
+//   constructor(brand) {
+//     super(brand);
+//   }
+// }
 
-const newBus = new Bus("lasta");
-console.log(newBus);
-console.log(newBus.getSpeed());
-newBus.fastUp();
-newBus.fastUp();
-newBus.fastUp();
-newBus.fastUp();
-newBus.fastUp();
-newBus.fastUp();
-newBus.fastUp();
-newBus.slowDown();
-console.log(newBus.getSpeed());
-newBus.stop();
-console.log(newBus.getSpeed());
+// const newBus = new Bus("lasta");
+// console.log(newBus);
+// console.log(newBus.getSpeed());
+// newBus.fastUp();
+// newBus.fastUp();
+// newBus.fastUp();
+// newBus.fastUp();
+// newBus.fastUp();
+// newBus.fastUp();
+// newBus.fastUp();
+// newBus.slowDown();
+// console.log(newBus.getSpeed());
+// newBus.stop();
+// console.log(newBus.getSpeed());
