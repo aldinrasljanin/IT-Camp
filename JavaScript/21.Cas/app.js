eaac149a0974276142e52eab99607e1b6735fbfa
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-03-21 22:40:33 +0100

    Callstack Asynchrone JavaScript

--- JavaScript/21.Cas/app.js
+++ JavaScript/21.Cas/app.js
@@ -8,6 +8,13 @@
 // console.log(8);
 // console.log(9);
 // console.log(10);
+
+// function numOne() {
+//   console.log(1);
+// }
+
+
+
 // Callstack last in first out
 // Callback que first in first out
 
