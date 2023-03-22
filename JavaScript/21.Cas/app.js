e1e2813cc53ae43de59f0c38d6cb410f32b5c4a4
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-03-22 23:27:09 +0100

    Asynchrone JavaScript with Callstack Function

--- JavaScript/21.Cas/app.js
+++ JavaScript/21.Cas/app.js
@@ -1,96 +1,96 @@
-console.log(1);
-console.log(2);
-console.log(3);
-console.log(4);
-console.log(5);
-console.log(6);
-console.log(7);
-console.log(8);
-console.log(9);
-console.log(10);
+// console.log(1);
+// console.log(2);
+// console.log(3);
+// console.log(4);
+// console.log(5);
+// console.log(6);
+// console.log(7);
+// console.log(8);
+// console.log(9);
+// console.log(10);
 
-Callstack: last in first out
-Callback que: first in first out
+// Callstack: last in first out
+// Callback que: first in first out
 
-First Example
-function numOne() {
-  console.log(1);
-}
+// First Example
+// function numOne() {
+//   console.log(1);
+// }
 
-function numTwo() {
-  console.log(2);
-  numOne();
-}
+// function numTwo() {
+//   console.log(2);
+//   numOne();
+// }
 
-function numThree() {
-  console.log(5);
-  numTwo();
-  console.log(3);
-}
+// function numThree() {
+//   console.log(5);
+//   numTwo();
+//   console.log(3);
+// }
 
-function numFive() {
-  console.log("first");
-  numThree();
-  console.log("a");
-}
+// function numFive() {
+//   console.log("first");
+//   numThree();
+//   console.log("a");
+// }
 
-numFive();
+// numFive();
 
-Second Example
-function sayMyName(name) {
-  console.log("Moje ime je " + name);
-}
+// Second Example
+// function sayMyName(name) {
+//   console.log("Moje ime je " + name);
+// }
 
-const nekaFunkcija = () => {
-  const myName = "aldin";
-  sayMyName(myName);
-};
+// const nekaFunkcija = () => {
+//   const myName = "aldin";
+//   sayMyName(myName);
+// };
 
-nekaFunkcija();
+// nekaFunkcija();
 
-Third Example
-function time() {
-  setTimeout(() => {
-    console.log("nesto");
-  }, 1000);
-}
+// Third Example
+// function time() {
+//   setTimeout(() => {
+//     console.log("nesto");
+//   }, 1000);
+// }
 
-function one() {
-  console.log(1);
-}
+// function one() {
+//   console.log(1);
+// }
 
-function two() {
-  setTimeout(() => {
-    console.log("bilo sta");
-  }, 2);
-}
+// function two() {
+//   setTimeout(() => {
+//     console.log("bilo sta");
+//   }, 2);
+// }
 
-function three() {
-  setTimeout(() => {
-    console.log(3);
-  }, 1);
-}
+// function three() {
+//   setTimeout(() => {
+//     console.log(3);
+//   }, 1);
+// }
 
-function neka() {
-  console.log("imas, nemas");
-}
+// function neka() {
+//   console.log("imas, nemas");
+// }
 
-console.log(2);
-console.log(2);
-console.log(2);
-two();
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
-console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// two();
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
+// console.log(2);
 
-//promise za sledeci cas //
+// //promise za sledeci cas //
