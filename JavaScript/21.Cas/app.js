60c6b78c4362aedb20e872f8ba675b0e31bc2279
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-03-25 11:40:38 +0100

    Asynchronous JavaScript,  Promise, Promise all and Data Fetch

--- JavaScript/21.Cas/app.js
+++ JavaScript/21.Cas/app.js
@@ -161,8 +161,8 @@
 // }
 
 function getData() {
-  fetch("https://jsonplaceholder.typicode.com/todos/1")
-    .then((response) => response.json())
-    .then((json) => console.log(json));
+  fetch("https://jsonplaceholder.typicode.com/todos/1").then((response) =>
+    console.log(response)
+  );
 }
 getData();
