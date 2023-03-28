826a74982e05113a31cbbb3cc02a79c0b8353dc9
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-03-28 11:53:10 +0200

    Fetch Data Exercise

--- JavaScript/22.Cas/app.js
+++ JavaScript/22.Cas/app.js
@@ -1,12 +1,13 @@
 const postsDiv = document.getElementById("posts");
-
+// console.log("#posts");
 fetch("https://jsonplaceholder.typicode.com/posts")
   .then((el) => el.json())
   .then((res) => {
     let skraceniPostovi = res.slice(0, 10);
     for (let post in skraceniPostovi) {
       const newParagraph = document.createElement("p");
-      newParagraph.innerText = post.title;
+      newParagraph.innerText = skraceniPostovi[post].title;
+
       postsDiv.append(newParagraph);
     }
   });
