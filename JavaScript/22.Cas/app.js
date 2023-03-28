f448b692a082753308713cfd8b608798eaa3b889
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-03-28 11:58:05 +0200

    Fetch Data Exercise

--- JavaScript/22.Cas/app.js
+++ JavaScript/22.Cas/app.js
@@ -1,13 +1,18 @@
 const postsDiv = document.getElementById("posts");
-// console.log("#posts");
-fetch("https://jsonplaceholder.typicode.com/posts")
-  .then((el) => el.json())
-  .then((res) => {
-    let skraceniPostovi = res.slice(0, 10);
-    for (let post in skraceniPostovi) {
-      const newParagraph = document.createElement("p");
-      newParagraph.innerText = skraceniPostovi[post].title;
+const dataButton = document.getElementById("buttonId");
+// console.log("postsDiv");
+let getData = () => {
+  fetch("https://jsonplaceholder.typicode.com/posts")
+    .then((el) => el.json())
+    .then((res) => {
+      let skraceniPostovi = res.slice(0, 10);
+      for (let post in skraceniPostovi) {
+        const newParagraph = document.createElement("p");
+        newParagraph.innerText = skraceniPostovi[post].title;
 
-      postsDiv.append(newParagraph);
-    }
-  });
+        postsDiv.append(newParagraph);
+      }
+    });
+};
+
+dataButton.addEventListener("click", () => getData());
