66d8c5afe62ab9c9c4d0fd9de691fb0c0388b0aa
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-03-28 12:19:23 +0200

    Fetch Data Exercise

--- JavaScript/22.Cas/app.js
+++ JavaScript/22.Cas/app.js
@@ -1,27 +1,27 @@
-const postsDiv = document.getElementById("posts");
-const dataButton = document.getElementById("buttonId");
-// console.log(postsDiv);
-let getData = () => {
-  fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((el) => el.json())
-    .then((res) => {
-      let skraceniPostovi = res.slice(0, 10);
-      for (let post in skraceniPostovi) {
-        const newParagraph = document.createElement("p");
-        newParagraph.innerText = skraceniPostovi[post].title;
+// const postsDiv = document.getElementById("posts");
+// const dataButton = document.getElementById("buttonId");
+// // console.log(postsDiv);
+// let getData = () => {
+//   fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then((el) => el.json())
+//     .then((res) => {
+//       let skraceniPostovi = res.slice(0, 10);
+//       for (let post in skraceniPostovi) {
+//         const newParagraph = document.createElement("p");
+//         newParagraph.innerText = skraceniPostovi[post].title;
 
-        postsDiv.append(newParagraph);
-      }
-    });
-};
+//         postsDiv.append(newParagraph);
+//       }
+//     });
+// };
 
-dataButton.addEventListener("click", () => getData());
+// dataButton.addEventListener("click", () => getData());
 
-let string = "Danas je lep dan";
-let reversedString = "";
+// let string = "Danas je lep dan";
+// let reversedString = "";
 
-for (let i = string.length - 1; i >= 0; i--) {
-  reversedString += string[i];
-}
+// for (let i = string.length - 1; i >= 0; i--) {
+//   reversedString += string[i];
+// }
 
-console.log(reversedString);
+// console.log(reversedString);
