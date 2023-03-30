40950874b4df514a253a12efbafc522e58cc3b06
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-03-30 12:18:49 +0200

    Exercise

---
+++ JavaScript/23.Cas/app.js
@@ -0,0 +1,25 @@
+const getData = () => {
+  fetch("https://dummyjson.com/carts/1")
+    .then((res) => res.json())
+    .then(console.log);
+};
+getData();
+
+async function getAveragePrice() {
+  const response = await fetch("https://dummyjson.com/carts/1");
+  const data = await response.json();
+  const prices = data.products.map((obj) => obj.price);
+  const sum = prices.reduce((prev, curr) => prev + curr);
+  const total = sum / prices.length;
+  console.log(total);
+}
+getAveragePrice();
+
+async function getDiscount() {
+  const response = await fetch("https://dummyjson.com/carts/1");
+  const data = await response.json();
+  const discounts = data.products.map((obj) => obj.discountPercentage);
+  const discount = discounts.filter((obj) => obj > 11.3);
+  console.log(discount);
+}
+getDiscount();
