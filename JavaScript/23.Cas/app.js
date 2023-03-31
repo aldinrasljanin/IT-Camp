b2833bbbd667fe1366d7b67833d1d8789631800e
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-03-31 23:26:35 +0200

    Data Fetch Exercise with Map, Filter and Reduce methods

--- JavaScript/23.Cas/app.js
+++ JavaScript/23.Cas/app.js
@@ -1,26 +1,26 @@
-// const getData = () => {
-//   fetch("https://dummyjson.com/carts/1")
-//     .then((res) => res.json())
-//     .then(console.log);
-// };
-// getData();
+const getData = () => {
+  fetch("https://dummyjson.com/carts/1")
+    .then((res) => res.json())
+    .then(console.log);
+};
+getData();
 
-// async function getAveragePrice() {
-//   const response = await fetch("https://dummyjson.com/carts/1");
-//   const data = await response.json();
-//   const prices = data.products.map((obj) => obj.price);
-//   const sum = prices.reduce((prev, curr) => prev + curr);
-//   const total = sum / prices.length;
-//   console.log(total);
-// }
-// getAveragePrice();
+async function getAveragePrice() {
+  const response = await fetch("https://dummyjson.com/carts/1");
+  const data = await response.json();
+  const prices = data.products.map((obj) => obj.price);
+  const sum = prices.reduce((prev, curr) => prev + curr);
+  const total = sum / prices.length;
+  console.log(total);
+}
+getAveragePrice();
 
-// async function getDiscount() {
-//   const response = await fetch("https://dummyjson.com/carts/1");
-//   const data = await response.json();
-//   const discounts = data.products.filter(
-//     (obj) => obj.discountPercentage > 11.3
-//   );
-//   console.log(discounts);
-// }
-// getDiscount();
+async function getDiscount() {
+  const response = await fetch("https://dummyjson.com/carts/1");
+  const data = await response.json();
+  const discounts = data.products.filter(
+    (obj) => obj.discountPercentage > 11.3
+  );
+  console.log(discounts);
+}
+getDiscount();
