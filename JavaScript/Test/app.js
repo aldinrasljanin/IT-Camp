8a4e4d0e0076ad45d65d8f5e99772630e3aeca49
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-04-05 21:16:41 +0200

    JavaScript test

--- JavaScript/Test/app.js
+++ JavaScript/Test/app.js
@@ -1,45 +1,45 @@
-// 1.Zadatak
-// FIZZBUZZ challange;
-// Ispisati brojeve od 0 do 100;
-// Ukoliko je broj deljiv sa 3 - ispisati "FIZZ"
-// Ukoliko je broj deljiv sa 3 - ispisati "BUZZ"
-// Ukoliko je broj deljiv sa 3 - ispisati "FIZZBUZZ";
+// // 1.Zadatak
+// // FIZZBUZZ challange;
+// // Ispisati brojeve od 0 do 100;
+// // Ukoliko je broj deljiv sa 3 - ispisati "FIZZ"
+// // Ukoliko je broj deljiv sa 3 - ispisati "BUZZ"
+// // Ukoliko je broj deljiv sa 3 - ispisati "FIZZBUZZ";
 
-const challange = () => {
-  for (let i = 0; i <= 100; i++) {
-    if (i % 3 === 0 && i % 5 === 0) {
-      console.log("FIZZBUZZ");
-    } else if (i % 3 === 0) {
-      console.log("FIZZ");
-    } else if (i % 5 === 0) {
-      console.log("BUZZ");
-    } else {
-      console.log(i);
-    }
-  }
-};
-challange();
+// const challange = () => {
+//   for (let i = 0; i <= 100; i++) {
+//     if (i % 3 === 0 && i % 5 === 0) {
+//       console.log("FIZZBUZZ");
+//     } else if (i % 3 === 0) {
+//       console.log("FIZZ");
+//     } else if (i % 5 === 0) {
+//       console.log("BUZZ");
+//     } else {
+//       console.log(i);
+//     }
+//   }
+// };
+// challange();
 
-// 2.Zadatak
-// Iz niza brojeva, izdvojiti sve parne brojeve, i duplirati ih,
+// // 2.Zadatak
+// // Iz niza brojeva, izdvojiti sve parne brojeve, i duplirati ih,
 
-const array = [3, 5, 8, 9, 10, 33, 56, 24, 93, 67, 34];
-const numbers = () => {
-  const evenArray = array.filter((num) => num % 2 === 0).map((num) => num * 2);
-  console.log(evenArray);
-};
-numbers();
+// const array = [3, 5, 8, 9, 10, 33, 56, 24, 93, 67, 34];
+// const numbers = () => {
+//   const evenArray = array.filter((num) => num % 2 === 0).map((num) => num * 2);
+//   console.log(evenArray);
+// };
+// numbers();
 
-// 3.Zadatak
-// Preuzmi podatke sa api-ja, i izdvojiti sve usere sa parnim id-jem;
-// API URL : https://jsonplaceholder.typecode.com/posts
+// // 3.Zadatak
+// // Preuzmi podatke sa api-ja, i izdvojiti sve usere sa parnim id-jem;
+// // API URL : https://jsonplaceholder.typecode.com/posts
 
-const getData = () => {
-  fetch("https://jsonplaceholder.typicode.com/posts")
-    .then((response) => response.json())
-    .then((data) => {
-      const userEvenId = data.filter((user) => user.id % 2 === 0);
-      console.log(userEvenId);
-    });
-};
-getData();
+// const getData = () => {
+//   fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then((response) => response.json())
+//     .then((data) => {
+//       const userEvenId = data.filter((user) => user.id % 2 === 0);
+//       console.log(userEvenId);
+//     });
+// };
+// getData();
