3c96f8f6f15da5c93f434693d7aa0d3d4cb22e33
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-05-08 20:01:13 +0200

    Scores App

---
+++ React/react-scores-app/src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+import "./App.css";
+class App extends React.Component {
+  render() {
+    return <div className="App"></div>;
+  }
+}
+<div className="app"></div>;
+<div className="scores-container">
+  <div className="score-card">
+    <h1>{}</h1>
+    <p>Home</p>
+    <button>Goal!</button>
+  </div>
+  ;
+</div>;
+export default App;
