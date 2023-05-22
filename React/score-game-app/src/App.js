3214d167565ad1a46f2d41fa8ab60f670efafd95
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-05-22 20:03:41 +0200

    react-game-app

--- React/score-game-app/src/App.js
+++ React/score-game-app/src/App.js
@@ -1,25 +1,70 @@
-import logo from "./logo.svg";
+import React from "react";
 import "./App.css";
+import ScoreCard from "./components/ScoreCard";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+// const [state,setState] = useState({
+//   homeScore: 0,
+//   awayScore: 0,
+// })
+
+class App extends React.Component {
+  state = {
+    homeScore: 0,
+    awayScore: 0,
+    gameover: false,
+  };
+
+  componentDidMount() {
+    console.log("DID MOUNT");
+    // this.setState({ homeScore: 0, guestScore: 0 });
+  }
+
+  componentDidUpdate() {
+    console.log("DID UPDATE");
+    if (this.state.gameover === false) {
+      // igra traje
+      if (this.state.homeScore === 5) {
+        alert("Home is winner");
+        this.setState({ gameover: true }); // prekini igru
+      }
+      if (this.state.awayScore === 5) {
+        alert("Guest is winner");
+        this.setState({ gameover: true });
+      }
+    }
+  }
+
+  render() {
+    return (
+      <div className="app">
+        <div>
+          <h1 className="title">Score Game</h1>
+        </div>
+
+        <div className="scores-container">
+          <ScoreCard
+            name="Home"
+            score={this.state.homeScore}
+            goal={() => {
+              if (this.state.gameover === false) {
+                this.setState({ homeScore: this.state.homeScore + 1 });
+              }
+            }}
+          />
+          <ScoreCard
+            name="Away"
+            score={this.state.awayScore}
+            goal={() => {
+              if (this.state.gameover === false) {
+                // igra traje
+                this.setState({ awayScore: this.state.awayScore + 1 });
+              }
+            }}
+          />
+        </div>
+      </div>
+    );
+  }
 }
 
 export default App;
