dc74e8670a9a927ad4e6b75fce4fb40ec92d6010
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-05-22 20:13:51 +0200

    react-game-app

--- React/score-game-app/src/App.js
+++ React/score-game-app/src/App.js
@@ -1,23 +1,35 @@
-// import React from "react";
-// import "./App.css";
-// import ScoreCard from "./components/ScoreCard";
-// class App extends React.Component {
-//   state = {
-//     scores=[],
-//     gameover: false,
-//   };
+import React from "react";
+import "./App.css";
+import ScoreCard from "./components/ScoreCard";
+class App extends React.Component {
+  state = {
+    scores: [],
+    gameover: false,
+    player: "",
+  };
 
-//   render() {
-//     return (
-//       <div className="app">
-//         <div className="scores-container">
-//           <ScoreCard name="Home" score={this.state.homeScore} goal={() => {}} />
-//           <ScoreCard name="Away" score={this.state.awayScore} goal={() => {}} />
-//         </div>
-//         inpu
-//       </div>
-//     );
-//   }
-// }
+  homeScoreGoal() {
+    const eventData = {
+      player: this.state.player,
+      team: "home",
+      event: "goal",
+    };
+    const newScores = this.state.scores;
+    newScores.push(eventData);
+    this.setState({ scores: newScores, player: "" });
+  }
 
-// export default App;
+  render() {
+    return (
+      <div className="app">
+        <div className="scores-container">
+          <ScoreCard name="Home" score={this.state.homeScore} goal={() => {}} />
+          <ScoreCard name="Away" score={this.state.awayScore} goal={() => {}} />
+        </div>
+        inpu
+      </div>
+    );
+  }
+}
+
+export default App;
