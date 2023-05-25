c94cffc8c6d35160831a1dd7238ca73f194d442c
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-05-25 23:47:02 +0200

    Score-Game-app

--- React/score-game-app/src/components/ScoreCard.js
+++ React/score-game-app/src/components/ScoreCard.js
@@ -1,3 +1,40 @@
+// import React from "react";
+// import "../App.css";
+
+// class ScoreCard extends React.Component {
+//   render() {
+//     return (
+//       <div className="score-card">
+//         <h1>{this.props.score}</h1>
+//         <p>{this.props.name}</p>
+//         <button
+//           onClick={(e) => {
+//             this.props.goal();
+//           }}
+//         >
+//           Goal!
+//         </button>
+//         <button
+//           onClick={(e) => {
+//             this.props.yellowCard();
+//           }}
+//         >
+//           Yellow Card
+//         </button>
+//         <button
+//           onClick={(e) => {
+//             this.props.redCard();
+//           }}
+//         >
+//           Red Card
+//         </button>
+//       </div>
+//     );
+//   }
+// }
+
+// export default ScoreCard;
+
 import React from "react";
 import "../App.css";
 
@@ -33,4 +70,34 @@ class ScoreCard extends React.Component {
   }
 }
 
-export default ScoreCard;
+const NewScoreCard = (props) => {
+  return (
+    <div className="score-card">
+      <h1>{props.score}</h1>
+      <p>{props.name}</p>
+      <button
+        onClick={(e) => {
+          props.goal();
+        }}
+      >
+        Goal!
+      </button>
+      <button
+        onClick={(e) => {
+          props.yellowCard();
+        }}
+      >
+        Yellow Card
+      </button>
+      <button
+        onClick={(e) => {
+          props.redCard();
+        }}
+      >
+        Red Card
+      </button>
+    </div>
+  );
+};
+
+export default NewScoreCard;
