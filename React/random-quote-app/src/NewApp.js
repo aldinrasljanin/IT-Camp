6f6ce7052ef21528df740d7d0463eb563e04cd79
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-05-25 23:43:05 +0200

    Random-Quote-app

--- React/random-quote-app/src/NewApp.js
+++ React/random-quote-app/src/NewApp.js
@@ -1,34 +1,59 @@
-// import React from "react";
-// import "./App.css";
+import React, { useState, useEffect } from "react";
+import "./App.css";
 
-// const App (props)=>{
-//     getRandomQuote() {
-//         fetch("https://js-course-server.onrender.com/quotes/get-random-quote")
-//           .then((res) => res.json())
-//           .then((data) => {
-//             this.setState({
-//               text: data.quoteText,
-//               author: data.quoteAuthor,
-//               likes: data.likes,
-//             });
-//           });
-//       }
-//     return (
-//         <div className="app">
-//           <div className="quote-wrapper">
-//             <div className="quote-card">
-//               <h3>Author:{this.state.author}</h3>
-//               <p className="quote-text">{this.state.text}</p>
-//               <p>Likes:{this.state.likes}</p>
-//             </div>
-//             <button
-//               onClick={() => {
-//                 this.getRandomQuote();
-//               }}
-//             >
-//               Get random quote
-//             </button>
-//           </div>
-//         </div>
-//       );
-// }
+const NewApp = () => {
+  const [state, setState] = useState({
+    author: "",
+    text: "",
+    likes: null,
+  });
+
+  const getRandomQuote = () => {
+    fetch("https://js-course-server.onrender.com/quotes/get-random-quote")
+      .then((res) => res.json())
+      .then((data) => {
+        setState({
+          text: data.quoteText,
+          author: data.quoteAuthor,
+          likes: data.likes,
+        });
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+  useEffect(() => {
+    console.log("componentDidMount");
+    getRandomQuote();
+  }, []);
+
+  useEffect(() => {
+    console.log("state is updated");
+  }, [state]);
+
+  useEffect(() => {
+    console.log("componentDidUpdate");
+  });
+
+  return (
+    <div className="app">
+      <div className="quote-wrapper">
+        <div className="quote-card">
+          <h3>Author: {state.author}</h3>
+          <p className="quote-text">{state.text}</p>
+          <p>Likes: {state.likes}</p>
+        </div>
+        <button
+          onClick={() => {
+            getRandomQuote();
+          }}
+        >
+          Get random quote
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default NewApp;
