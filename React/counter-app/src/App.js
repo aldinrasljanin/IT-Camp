e37e90a750c3c18d5fb01c4299baafe029470680
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-05-25 23:40:44 +0200

    Counter-app

--- React/counter-app/src/App.js
+++ React/counter-app/src/App.js
@@ -4,28 +4,29 @@ import "./App.css";
 class App extends React.Component {
   state = { counter: 0 };
 
-  incrementCounter = () => {
-    this.setState({ counter: this.state.counter + 1 });
-  };
-
-  resetCounter = () => {
-    this.setState({ counter: 0 });
-  };
-
   render() {
+    ///
+    ///
+    ///
     return (
       <div className="App">
-        <div className="container">
-          <h1>{this.state.counter}</h1>
-          <div className="button-wrapper">
-            <div className="count-button" onClick={this.incrementCounter}>
-              <p>+</p>
-            </div>
-            <div className="reset-button" onClick={this.resetCounter}>
-              <p>Reset</p>
-            </div>
+        <header className="App-header">
+          <h1 className="count-number">{this.state.counter}</h1>
+          <div className="buttons-wrapper">
+            <div
+              className="count-button"
+              onClick={() => {
+                this.setState({ counter: this.state.counter + 1 });
+              }}
+            ></div>
+            <div
+              className="reset-button"
+              onClick={() => {
+                this.setState({ counter: 0 });
+              }}
+            ></div>
           </div>
-        </div>
+        </header>
       </div>
     );
   }
--- React/counter-app/src/NewApp.js
+++ React/counter-app/src/NewApp.js
@@ -1,20 +1,28 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 
 const App = (props) => {
+  const [counter, setCounter] = useState(0);
+
   return (
     <div className="App">
-      <div className="container">
-        <h1>{this.state.counter}</h1>
-        <div className="button-wrapper">
-          <div className="count-button" onClick={this.incrementCounter}>
-            <p>+</p>
-          </div>
-          <div className="reset-button" onClick={this.resetCounter}>
-            <p>Reset</p>
-          </div>
+      <header className="App-header">
+        <h1 className="count-number">{counter}</h1>
+        <div className="buttons-wrapper">
+          <div
+            className="count-button"
+            onClick={() => {
+              setCounter(counter + 1);
+            }}
+          ></div>
+          <div
+            className="reset-button"
+            onClick={() => {
+              setCounter(0);
+            }}
+          ></div>
         </div>
-      </div>
+      </header>
     </div>
   );
 };
