fa22d641e55eff62cfb48f95bea6ea32ca0e10cc
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-05-26 20:08:04 +0200

    Instagram-Clone-app

---
+++ React/instagram-clone-app/src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./App.css";
+import data from "./data.json";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="posts">
+        <div className="post"></div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
