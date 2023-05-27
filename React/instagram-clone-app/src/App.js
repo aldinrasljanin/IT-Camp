eeccfd603d117e2c503273db328d18e457d114c0
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-05-27 23:51:03 +0200

    Instagram-Clone-app

--- React/instagram-clone-app/src/App.js
+++ React/instagram-clone-app/src/App.js
@@ -1,12 +1,39 @@
-import React from "react";
 import "./App.css";
-// import data from "./data.json";
+import mydata from "./data.json";
+import Post from "./components/Post";
+import { useState } from "react";
 
 function App() {
+  const myUsername = "mmuminovic";
+  const [data, setData] = useState(mydata);
+
+  const likeHandler = (postId) => {
+    const newData = data.map((item, index) => {
+      if (item.id === postId) {
+        let likes = item.likes;
+        if (likes.includes(myUsername)) {
+          likes = likes.filter((u) => u !== myUsername);
+        } else {
+          likes.push(myUsername);
+        }
+        item.likes = likes;
+      }
+      return item;
+    });
+    setData(newData);
+  };
+
   return (
     <div className="App">
       <div className="posts">
-        <div className="post"></div>
+        {data.map((post) => (
+          <Post
+            key={post.id}
+            post={post}
+            likeHandler={likeHandler}
+            myUsername={myUsername}
+          />
+        ))}
       </div>
     </div>
   );
