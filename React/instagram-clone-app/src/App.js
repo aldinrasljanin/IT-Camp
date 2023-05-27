c48ac89e0c599b8898a8806c0315d0d4cd730af2
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-05-28 00:00:18 +0200

    Instagram-Clone-app

--- React/instagram-clone-app/src/App.js
+++ React/instagram-clone-app/src/App.js
@@ -1,42 +1,42 @@
-// import "./App.css";
-// import mydata from "./data.json";
-// import Post from "./components/Post";
-// import { useState } from "react";
+import "./App.css";
+import mydata from "./data.json";
+import Post from "./components/Post";
+import { useState } from "react";
 
-// function App() {
-//   const myUsername = "mmuminovic";
-//   const [data, setData] = useState(mydata);
+function App() {
+  const myUsername = "mmuminovic";
+  const [data, setData] = useState(mydata);
 
-//   const likeHandler = (postId) => {
-//     const newData = data.map((item, index) => {
-//       if (item.id === postId) {
-//         let likes = item.likes;
-//         if (likes.includes(myUsername)) {
-//           likes = likes.filter((u) => u !== myUsername);
-//         } else {
-//           likes.push(myUsername);
-//         }
-//         item.likes = likes;
-//       }
-//       return item;
-//     });
-//     setData(newData);
-//   };
+  const likeHandler = (postId) => {
+    const newData = data.map((item, index) => {
+      if (item.id === postId) {
+        let likes = item.likes;
+        if (likes.includes(myUsername)) {
+          likes = likes.filter((u) => u !== myUsername);
+        } else {
+          likes.push(myUsername);
+        }
+        item.likes = likes;
+      }
+      return item;
+    });
+    setData(newData);
+  };
 
-//   return (
-//     <div className="App">
-//       <div className="posts">
-//         {data.map((post) => (
-//           <Post
-//             key={post.id}
-//             post={post}
-//             likeHandler={likeHandler}
-//             myUsername={myUsername}
-//           />
-//         ))}
-//       </div>
-//     </div>
-//   );
-// }
+  return (
+    <div className="App">
+      <div className="posts">
+        {data.map((post) => (
+          <Post
+            key={post.id}
+            post={post}
+            likeHandler={likeHandler}
+            myUsername={myUsername}
+          />
+        ))}
+      </div>
+    </div>
+  );
+}
 
-// export default App;
+export default App;
