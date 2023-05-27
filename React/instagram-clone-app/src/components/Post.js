c0da64537fa89fb27e9ef1f7c7d7bf179b4a1f56
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-05-27 23:49:13 +0200

    liked button

---
+++ React/instagram-clone-app/src/components/Post.js
@@ -0,0 +1,53 @@
+import React from "react";
+import LikedIcon from "../assets/liked.png";
+import UnlikedIcon from "../assets/unliked.png";
+
+const Post = (props) => {
+  // props = { post: {...} }
+  const isPostLikedByMe = props.post.likes.includes(props.myUsername);
+  let likeButtonImg = UnlikedIcon;
+  if (isPostLikedByMe) {
+    likeButtonImg = LikedIcon;
+  }
+
+  return (
+    <div className="post">
+      <div>
+        <img className="post-img" src={props.post.imageUrl} />
+      </div>
+      <div className="post-buttons">
+        <button
+          onClick={() => {
+            props.likeHandler(props.post.id);
+          }}
+          style={{ paddingRight: "10px", paddingLeft: "10px" }}
+        >
+          <img src={likeButtonImg} width="10px" />
+        </button>
+        <button
+          onClick={() => {
+            console.log("Comment");
+          }}
+        >
+          Comment
+        </button>
+        <button
+          onClick={() => {
+            console.log("Share");
+          }}
+        >
+          Share
+        </button>
+      </div>
+      <div>
+        <p className="post-likes">{props.post.likes.length} likes</p>
+        <p className="post-description">
+          <span className="username">{props.post.username}</span>{" "}
+          <span>{props.post.description}</span>
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
