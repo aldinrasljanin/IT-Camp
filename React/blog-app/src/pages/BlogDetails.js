b06c0bb89be247586da04ff28435fa09e03a8e4b
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-05-29 19:52:48 +0200

    Blog-app

---
+++ React/blog-app/src/pages/BlogDetails.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function BlogDetails() {
+  return <div>BlogDetails</div>;
+}
+
+export default BlogDetails;
---
+++ React/blog-app/src/pages/Blogs.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+function Blogs() {
+  return (
+    <div>
+      <h3>This is my blog title</h3>
+      <p>This blog is about programming</p>
+      <button>Blog details</button>
+    </div>
+  );
+}
+
+export default Blogs;
