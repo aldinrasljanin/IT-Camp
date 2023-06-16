b19532f6b447f9f3a0907d417c4b2a74aee20189
Aldin <110719639+aldinrasljanin@users.noreply.github.com>
2023-06-16 16:45:06 +0200

    edit quote page

--- React/react-quote-app/src/App.js
+++ React/react-quote-app/src/App.js
@@ -1,10 +1,11 @@
-import React from "react";
 import "./App.css";
 import { Routes, Route, BrowserRouter } from "react-router-dom";
 import AllQuotes from "./pages/AllQuotes/AllQuotes";
 import QuoteDetails from "./pages/QuoteDetails/QuoteDetails";
-import Login from "./pages/Login/login";
-import Register from "./pages/Register/register";
+import Login from "./pages/Login/Login";
+import Register from "./pages/Register/Register";
+import AddQuote from "./pages/AddQuote/AddQuote";
+import EditQuote from "./pages/EditQuote/EditQuote";
 
 function App() {
   return (
@@ -12,8 +13,10 @@ function App() {
       <Routes>
         <Route path="/" element={<AllQuotes />} />
         <Route path="/quote/:id" element={<QuoteDetails />} />
+        <Route path="/quote/:id/edit" element={<EditQuote />} />
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
+        <Route path="/add-quote" element={<AddQuote />} />
       </Routes>
     </BrowserRouter>
   );
